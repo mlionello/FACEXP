af2865d74c9ccb81fd121e8f860bde83a4db12dc
mlionello <matteo.lionello@imtlucca.it>
2023-06-23 00:48:28 +0200

    pca output correction

--- code/RAVNESS_analyser.py
+++ code/RAVNESS_analyser.py
@@ -1,13 +1,9 @@
 import pickle
 
 import numpy as np
-from sklearn.model_selection import KFold, StratifiedKFold, StratifiedShuffleSplit, cross_validate
+from sklearn.model_selection import StratifiedKFold, cross_validate
 from sklearn.neighbors import KNeighborsClassifier as knnc
-from sklearn.svm import SVC
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-import matplotlib.pyplot as plt
-from sklearn.decomposition import FastICA
+
 from sklearn.decomposition import PCA
 
 pca_n = 20
