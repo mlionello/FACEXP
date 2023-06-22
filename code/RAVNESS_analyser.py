7f46fcba80b1950dabbac2bc7a675c696c785752
mlionello <matteo.lionello@imtlucca.it>
2023-06-22 23:50:40 +0200

    ! in test indices & correction!!!!

--- code/RAVNESS_analyser.py
+++ code/RAVNESS_analyser.py
@@ -42,8 +42,8 @@ mod = labels[:, mod_col_ind]
 y = np.reshape(emotions, (-1,))
 
 valid_indices = (mod == 2)
-valid_indices = valid_indices & (emotions>2) #& (ch == 2) & (intensity == 2)
-#valid_indices = valid_indices & (rep == 1)
+valid_indices = valid_indices & (emotions>2) & (ch == 2) & (intensity == 2)
+valid_indices = valid_indices & (rep == 1)
 valid_indices = valid_indices & (intensity == 2)
 
 unique_actors = np.unique(actors)
