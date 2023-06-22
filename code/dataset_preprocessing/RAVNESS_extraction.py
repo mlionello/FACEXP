99b715eeb20677a76600c41ff50c9dc039219bea
mlionello <matteo.lionello@imtlucca.it>
2023-06-22 19:12:22 +0200

    RAVNESS features and labels extraction

--- code/dataset_preprocessing/RAVNESS_extraction.py
+++ code/dataset_preprocessing/RAVNESS_extraction.py
@@ -1,15 +1,16 @@
 import argparse
+import os
 
-import pandas as pd
 from pathlib import Path
 import h5py
 from scipy.spatial.distance import pdist
 import numpy as np
 
 def get_dataset(inpath, outfolder, refh5):
+    if not outfolder.is_dir():
+        os.mkdir(outfolder)
     column_names = ["Modality", "Channel", "Emotion", "Intensity",
                     "Statement", "Repetition", "Actor"]
-    df = pd.DataFrame(columns=column_names)
     print("available entries: ", column_names)
 
     meshatrest = np.load(refh5)
@@ -28,13 +29,13 @@ def get_dataset(inpath, outfolder, refh5):
             features.append(pdist(np.mean(data_in["v"], 0)) - pdist_rest)
 
     labels = {"file_ids": file_ids, "labels": labels, "column_names": column_names}
-    np.save('features', features)
-    np.save('labels', labels)
-    a = 1
+    np.save( outfolder / 'features', features)
+    np.save( outfolder / 'labels', labels)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='h5 to dataframes and npy for features and labels')
-    parser.add_argument('--input', type=str, default='../../mediapipe/test', help='input_folder')
+    parser.add_argument('--input', type=str, help='input_folder')
     parser.add_argument('--ref', type=str, default='./refAtRest.npy', help='ref h5 template at rest')
     parser.add_argument('--out', type=str, default='./', help='output folder')
     args = parser.parse_args()
