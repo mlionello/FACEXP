3a0edff404b69086896039e5aaa861ca15b8a13f
mlionello <matteo.lionello@imtlucca.it>
2023-06-28 15:51:12 +0200

    pedfe ready for analysis

---
+++ facexp_models/scripts/ravdess_plot_res.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+from pathlib import Path
+
+trch=0
+trrep=0
+tstrep=0
+tstintensity = 0
+scores2plot =[]
+column_names = []
+label=[]
+for trch in [0, 1]:
+    for trintensity in [0, 1]:
+        label.append(f"trch_{trch}-trintensity_{trintensity}")
+        filepath = Path(f'../results/ravdess_results/trch_{trch}_trrep_{trrep}_'
+                   f'trintensity_{trintensity}_tstrep_{tstrep}_tstintensity_{tstintensity}')
+        with open(filepath / 'data.pkl', 'rb') as ofile:
+            data = pickle.load(ofile)
+
+        scorestot = [data["scorestot"]["test_score"], data["scorestot"]["train_score"]]
+        test_scores = data["test_score"]
+        preds = data["preds"]
+        scorestot = np.mean(scorestot, axis=1)
+        test_scores = np.array(test_scores)
+        preds = np.array(preds)
+        scores2plot.append(test_scores)
+        column_names.append(f'tr_channel_{trch}-tr_intensity_{trintensity}')
+
+plt.violinplot(scores2plot, showextrema=False, showmeans=True)
+[plt.plot(np.ones((24, 1))+j, scores2plot[j], 'o', label=label[j]) for j in range(len(scores2plot))]
+plt.legend()
+plt.show()
