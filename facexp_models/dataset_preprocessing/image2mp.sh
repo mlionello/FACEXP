076e90c3230b3382e30beadf358a885e3307398e
mlionello <lionello.matteo@gmail.com>
2023-09-27 16:11:06 +0000

    m

--- facexp_models/dataset_preprocessing/image2mp.sh
+++ facexp_models/dataset_preprocessing/image2mp.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
 
-homepath="/home/matteo/Pictures/Screenshots/"
+homepath="/home/matteo.lionello/Pictures/"
 
 for filename in "$homepath"/*; do
     if [ -d "$filename" ] || [[ "$filename" != *.jpg ]] && [[ "$filename" != *.png ]]; then
         continue
     fi
+    file_stem=$(echo "$filename" | sed -r "s/.+\/(.+)\..+/\1/")
     echo "$filename"
-    python ../vid2h5/get_h5_from_folder.py --input "$filename" --out ./tmpresampled.jpg --model img2vid
-    ffmpeg -loop 1 -i ./tmpresampled.jpg -t 1 tmpvidout.mp4
-    python ../vid2h5/get_h5_from_folder.py --input tmpvidout.mp4 --out ./tmp
+    python ../vid2h5/get_h5_from_folder.py --input "$filename" --out ./"$file_stem".jpg --model img2vid
+    ffmpeg -loop 1 -i ./"$filename".jpg -t 1 "$file_stem".mp4
+    python ../vid2h5/get_h5_from_folder.py --input "$file_stem".mp4 --out ./tmp
 done
